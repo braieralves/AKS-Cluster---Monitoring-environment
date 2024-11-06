b36110a83f1208cc855eab1b384a2e970a28147e
Braier Alexandre <balexandre@microsoft.com>
2024-11-06 10:53:16 +0000

    Update create aks with monitoring environment.sh

--- create aks with monitoring environment.sh
+++ create aks with monitoring environment.sh
@@ -16,9 +16,9 @@
 # 3 - Copy the content of this script
 # 4 - Paste the copied content in the file "aks-monitoring.sh" - Right click and "Paste"
 # 5 - Save the content - Commmand "CTRL + o"
-# 5 - Give teh execution permission to the file "aks-monitoring.sh" - Command "chmod +x aks-monitoring.sh"
-# 6 - Execute the script with command "sh aks-monitoring.sh"
-# 7 - Inform the names of the resources
+# 6 - Give teh execution permission to the file "aks-monitoring.sh" - Command "chmod +x aks-monitoring.sh"
+# 7 - Execute the script with command "sh aks-monitoring.sh"
+# 8 - Inform the names of the resources
 
 # This take some minutes to create the complete environment
 
